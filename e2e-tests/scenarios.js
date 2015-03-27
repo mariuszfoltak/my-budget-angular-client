cdadbb32c529da11720c5e14ee0f5427adcd5948
Mariusz Foltak <mariuszfoltak@coi.gov.pl>
2015-03-27 15:42:31 +0100

    Add e2e tests for authentication

--- e2e-tests/scenarios.js
+++ e2e-tests/scenarios.js
@@ -2,45 +2,25 @@
 
 /* https://github.com/angular/protractor/blob/master/docs/toc.md */
 
-describe('my app', function () {
+describe('Authentication module', function () {
 
     browser.get('index.html');
-    browser.executeScript('window.localStorage.username = "mariusz"');
+    browser.executeScript('window.localStorage.removeItem("username");');
 
 
-    it('should automatically redirect to /app/ui/transactions when location hash/fragment is empty', function () {
-        browser.get('index.html');
+    it('should automatically redirect to /access/login when location hash/fragment is empty', function () {
+        browser.setLocation('');
+        browser.sleep(500);
+        expect(browser.getLocationAbsUrl()).toMatch("/access/signin");
+    });
+
+    it('should automatically redirect to /app/ui/transactions after correctly login', function () {
+        browser.setLocation('/access/login');
+        element(by.model('credentials.username')).sendKeys('mariusz');
+        element(by.model('credentials.password')).sendKeys('abc123');
+        element(by.id('signin')).click();
         browser.sleep(500);
         expect(browser.getLocationAbsUrl()).toMatch("/app/ui/transactions");
     });
-
-
-    //describe('view1', function() {
-    //
-    //  beforeEach(function() {
-    //    browser.get('index.html#/view1');
-    //  });
-    //
-    //
-    //  it('should render view1 when user navigates to /view1', function() {
-    //    expect(element.all(by.css('[ng-view] p')).first().getText()).
-    //      toMatch(/partial for view 1/);
-    //  });
-    //
-    //});
-
-
-    //describe('view2', function() {
-    //
-    //  beforeEach(function() {
-    //    browser.get('index.html#/view2');
-    //  });
-    //
-    //
-    //  it('should render view2 when user navigates to /view2', function() {
-    //    expect(element.all(by.css('[ng-view] p')).first().getText()).
-    //      toMatch(/partial for view 2/);
-    //  });
-    //
-    //});
 });
+;
--- src/modules/authentication/signin.js
+++ src/modules/authentication/signin.js
@@ -11,7 +11,7 @@ app
                 AuthService.login(credentials);
                 $http.defaults.headers.common["Authorization-User"] = credentials.username;
                 $http.defaults.headers.common["Authorization-Password"] = credentials.password;
-                $state.go('app.ui.portlet');
+                $state.go('app.ui.transactions');
             };
         }])
 ;
